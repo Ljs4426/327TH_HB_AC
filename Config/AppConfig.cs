b7f4ec22e5b72e9680a0e67b97e950627de287f0
Ljs4426 <152663571+Ljs4426@users.noreply.github.com>
2026-02-19 20:01:07 -0600

    Update CurrentVersion to 2.0.0

--- Config/AppConfig.cs
+++ Config/AppConfig.cs
@@ -4,7 +4,7 @@ namespace Hawkbat.Config
 {
     public static class AppConfig
     {
-        public const string CurrentVersion = "1.0.0";
+        public const string CurrentVersion = "2.0.0";
         public const string BuildTimeHash = "DISABLED";
     }
 }
